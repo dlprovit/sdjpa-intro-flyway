ec1522025ee0124b9b03ca53ea889246b37f1f5b
dlprovit <damion.provitola@gmail.com>
2024-01-27 20:31:16 -0800

    Assignment 5 completed

--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,6 +1,12 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.domain.AuthorUuid;
+import guru.springframework.sdjpaintro.domain.BookUuid;
+import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import guru.springframework.sdjpaintro.repositories.BookUuidRepository;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +14,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -23,6 +32,12 @@ public class MySQLIntegrationTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
     @Test
     void testMySQL() {
         long countBefore = bookRepository.count();
@@ -30,6 +45,29 @@ public class MySQLIntegrationTest {
 
     }
 
+    @Test
+    void testAuthorUuid() {
+
+        AuthorUuid authorUuid = new AuthorUuid();
+        AuthorUuid savedAuthorUuid = authorUuidRepository.save(authorUuid);
+        assertThat(authorUuid).isNotNull();
+        assertThat(authorUuid.getId()).isNotNull();
+
+        AuthorUuid actualAuthorUuid = authorUuidRepository.getById(savedAuthorUuid.getId());
+        assertThat(actualAuthorUuid.getId()).isNotNull();
+
+    }
+
+    @Test
+    void testBookUuid() {
+
+        BookUuid savedBookUuid = bookUuidRepository.save(new BookUuid());
+        assertThat(savedBookUuid).isNotNull();
+        assertThat(savedBookUuid.getId()).isNotNull();
+
+        BookUuid actualBookUuid = bookUuidRepository.getById(savedBookUuid.getId());
+        assertThat(actualBookUuid).isNotNull();
+    }
 }
 
 
